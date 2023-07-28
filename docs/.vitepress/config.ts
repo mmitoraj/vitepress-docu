2a47e7ce0bfbbfcc9fe52ccc31c9d3aa195c327c
mmitoraj <malgorzata.swieca@sap.com>
2023-07-28 12:28:23 +0200

    Remove base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/vitepress-docu/',
   title: "Test docu with VitePress",
   description: "Vitepress docu",
   themeConfig: {
